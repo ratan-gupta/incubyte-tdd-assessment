776e493908afcca15254bad55ab6bca123e702e3
unknown <ratan9556@gmail.com>
2022-01-06 18:00:00 +0530

    Passed test: adding1Num()

--- src/com/demo/tdd/Calculator.java
+++ src/com/demo/tdd/Calculator.java
@@ -1,9 +1,10 @@
 package com.demo.tdd;
 
 public class Calculator {
-    public int add(String str) {
+    public int add(String numbers) {
         int sum = 0;
-        if(str.length() == 0) return 0;
+        if(numbers.length() == 0) return 0;
+        if(numbers.length() == 1) return numbers.charAt(0) - '0';
 
         return sum;
     }
