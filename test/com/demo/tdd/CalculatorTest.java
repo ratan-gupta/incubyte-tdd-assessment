9a7d5e209ac4528bdd31c72e007bf86223ea6bef
unknown <ratan9556@gmail.com>
2022-01-06 17:39:54 +0530

    Failed test: adding0Num()

--- test/com/demo/tdd/CalculatorTest.java
+++ test/com/demo/tdd/CalculatorTest.java
@@ -2,12 +2,21 @@ package com.demo.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CalculatorTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void adding0Num() {
+        // arrange
+        Calculator calculator = new Calculator();
+        int expectedSum = 0;
+
+        // act
+        int actualSum = calculator.add("");
+
+        // assert
+        assertEquals(expectedSum, actualSum);
     }
 }
